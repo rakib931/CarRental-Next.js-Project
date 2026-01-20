5fcd81fc72bef3fad2aa90e5397983b4958c5bc1
rakib931 <rakibhossain455673@gmail.com>
2026-01-20 13:08:30 +0600

    vehicle page edited

--- src/app/vehicals/page.jsx
+++ src/app/vehicals/page.jsx
@@ -1,10 +1,6 @@
 import VehicalCard from "@/Component/VehicalCard";
-import Link from "next/link";
 
-export default async function AllVehicals() {
-  const data = await fetch("http://localhost:5000/vehicals");
-  const vehicals = await data.json();
-  // console.log(vehicals);
+export default function AllVehicals({ vehicals }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-4 my-10 px-5">
       {vehicals.map((vehicle) => (
@@ -12,4 +8,10 @@ export default async function AllVehicals() {
       ))}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:5000/vehicals");
+  const vehicals = await res.json();
+  return { props: { vehicals } };
 }
